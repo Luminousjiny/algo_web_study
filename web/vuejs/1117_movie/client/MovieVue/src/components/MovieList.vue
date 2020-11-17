d14a6e31ab51dc873a12a7b108db530697931842
dovvn <jdaun.dev@gmail.com>
2020-11-18 02:12:50 +0900

    ADD MovieVue

---
+++ web/vuejs/1117_movie/client/MovieVue/src/components/MovieList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+      <div id="movieList">
+      <table border="1px">
+        <thead>
+          <th>번호</th>
+          <th>영화 제목</th>
+          <th>영화 개봉일</th>
+          <th>영화 감독</th>
+        </thead>
+        <tr v-for="m in movies.movieList" v-bind:key="m.mno">
+          <td> <router-link :to="{name: 'MovieRead', params: {mno: m.mno}}">{{m.mno}}</router-link></td>
+          <td>{{m.mtitle}}</td>
+          <td>{{m.mdate}}</td>
+          <td>{{m.mdirector}}</td>
+        </tr>
+      </table>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    created(){
+        axios.get('http://localhost:8097/movieboard/api/movies', {
+          params: {
+            page: 2
+          }
+        })
+        .then((response)=>{
+            this.movies = response.data;
+        })
+        .catch((ex)=>{
+            console.log(ex);
+        })
+    },
+    data() {
+        return {
+            movies : [],
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ web/vuejs/1117_movie/client/MovieVue/src/components/MovieUpdate.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+       <table>
+        <tr>
+          <th>영화 제목</th>
+          <td><input type="text" name="mtitle" v-model="movie.mtitle" required /></td>
+        </tr>
+        <tr>
+          <th>영화 개봉일</th>
+          <td><input type="date" name="mdate" v-model="movie.mdate" /></td>
+        </tr>
+        <tr>
+          <th>영화 감독</th>
+          <td><input type="text" name="mdirector" v-model="movie.mdirector" /></td>
+        </tr>
+        <tr>
+          <th>영화 줄거리</th>
+          <td><textarea rows="10" cols="50" name="mcontent" v-model="movie.mcontent" /></td>
+        </tr>
+      </table>
+      <button><a href="#" @click="update">수정하기</a></button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+        return{
+            movies : [],
+          movie  : {
+            mno: this.$route.params.movie.mno,
+            mtitle: this.$route.params.movie.mtitle,
+            mdate: this.$route.params.movie.mdate,
+            mdirector: this.$route.params.movie.mdirector,
+            mcontent: this.$route.params.movie.mcontent,
+          },
+            }
+        },
+        methods: {
+          update(){
+            axios.put('http://localhost:8097/movieboard/api/movies/'+this.$route.params.movie.mno, this.movie )
+            .then(()=>{
+                this.$router.push('/MovieList'); //영화목록으로 이동
+            })
+            .catch((ex)=>{
+                alert('error'+ex);
+            })
+          }
+        },
+    
+}
+</script>
+
+
+<style>
+      table {
+        margin: auto;
+        width: 1000px;
+      }
+
+      th{
+          width: 300px;
+      }
+</style>
