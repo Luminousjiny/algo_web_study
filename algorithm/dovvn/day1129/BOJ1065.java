484483f21654ebd3afa89f3ad21e950608b177a2
dovvn <jdaun.dev@gmail.com>
2020-12-01 22:50:07 +0900

    Modify 한수

--- algorithm/dovvn/day1129/BOJ1065.java
+++ algorithm/dovvn/day1129/BOJ1065.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 //한수
@@ -15,19 +13,13 @@ public class Main {
 				Ans++;
 				continue;
 			}
-			List<Integer> arr = new ArrayList<>();
 			String s = Integer.toString(i);
 			
-			for(int j=0; j<s.length(); j++) {
-				//각 자릿수 리스트에 넣기
-				arr.add((int) (s.charAt(j)-'0'));
-			}
-			
 			//등차수열인지 확인
-			int d = arr.get(0)-arr.get(1);
+			int d = (s.charAt(0)-'0')-(s.charAt(1)-'0');
 			boolean isflag = true; //등차수열이면 true
-			for(int j=1; j<arr.size()-1; j++) {
-				if((arr.get(j)-arr.get(j+1)) == d) continue;
+			for(int j=1; j<s.length()-1; j++) {
+				if(((s.charAt(j)-'0')-(s.charAt(j+1)-'0')) == d) continue;
 				else {
 					isflag = false;
 					break;
