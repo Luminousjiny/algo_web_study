786c664d1fed42270c33a02f3428b45e0dffc65a
Luminousjiny <wlsdk400@naver.com>
2020-12-04 02:30:16 +0900

    Algo_update 2

---
+++ algorithm/jiny/1. 입출력/BOJ10171.java
@@ -0,0 +1,10 @@
+package day1201;
+
+public class BOJ10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
