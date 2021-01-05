a7efe82abb5ffe49e49265d84102d778dc1c3ad4
Junghyun100 <qorwjdgus8951@gmail.com>
2021-01-05 12:38:06 +0900

    Update BOJ10816.java

--- algorithm/100/이분탐색/BOJ10816.java
+++ algorithm/100/이분탐색/BOJ10816.java
@@ -8,6 +8,18 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/*
+
+Upper Bound와 Lower Bound는 이진탐색을 활용한 알고리즘
+UpperBound와 LowerBound는 원하는 값을 초과하는 첫번째 위치, 원하는 값 이상의 첫번째 위치를 반환 함
+
+정렬된 배열에서 찾고자 하는 값 key가 있을 때,
+Upper Bound: key보다 큰 첫번째 위치(초과)를 반환.
+Lower Bound: key보다 크거나 같은 첫번째 위치(이상)를 반환.
+
+
+*/
+
 public class BOJ10816 {
 	static int[] array;
 	public static void main(String[] args) throws IOException {
