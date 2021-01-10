e8f672dea125fe8ebb7d846ac760009677da9cf8
dovvn <jdaun.dev@gmail.com>
2021-01-11 02:03:37 +0900

    Add DP 1904,1932,2579,11054

---
+++ algorithm/dovvn/18. 동적계획법1/BOJ1904.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//01타일
+//피보나치처럼 1->2->3->5->8->...
+public class Main {
+
+    static int N;
+    static int[] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        dp = new int[1000001];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=N; i++) dp[i] = (dp[i-2] + dp[i-1]) % 15746;
+        System.out.println(dp[N]);
+    }
+}
